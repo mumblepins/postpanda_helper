ff7ccee6d7e67894916cb3d092f346e15c832c19
Sullivan <57496265+ds-mn@users.noreply.github.com>
2021-03-05 11:13:08 -0600

    Lots of updates, additions, docs, PandasWriter,PandasReader....

---
+++ docs/source/postpanda_helper.rst
@@ -0,0 +1,61 @@
+postpanda\_helper package
+=========================
+
+Submodules
+----------
+
+postpanda\_helper.geo\_helpers module
+-------------------------------------
+
+.. automodule:: postpanda_helper.geo_helpers
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+postpanda\_helper.pandas\_reader module
+---------------------------------------
+
+.. automodule:: postpanda_helper.pandas_reader
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+postpanda\_helper.pandas\_writer module
+---------------------------------------
+
+.. automodule:: postpanda_helper.pandas_writer
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+postpanda\_helper.pd\_helpers module
+------------------------------------
+
+.. automodule:: postpanda_helper.pd_helpers
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+postpanda\_helper.psql\_helpers module
+--------------------------------------
+
+.. automodule:: postpanda_helper.psql_helpers
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+postpanda\_helper.select\_sert module
+-------------------------------------
+
+.. automodule:: postpanda_helper.select_sert
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: postpanda_helper
+   :members:
+   :undoc-members:
+   :show-inheritance:
--- postpanda_helper/pd_helpers.py
+++ postpanda_helper/pd_helpers.py
@@ -2,6 +2,7 @@
 import random
 import string
 from contextlib import contextmanager
+from datetime import date
 from typing import Union
 
 import numpy as np
@@ -9,8 +10,12 @@ import pandas as pd
 from psycopg2._psycopg import adapt
 from psycopg2._range import DateRange, DateTimeTZRange, DateTimeRange, NumericRange
 
+from postpanda_helper.geo_helpers import fill_geoseries
+
 _NA_FILL_INTEGER = np.uint64(random.SystemRandom().getrandbits(64)).astype(np.int64)
 _NA_FILL_OBJECT = "na_val_" + "".join(random.SystemRandom().choices(string.ascii_lowercase, k=8))
+
+DATE_FORMATS = {4: "%Y", 6: "%Y%m", 8: "%Y%m%d"}
 
 
 def downcast(data: pd.Series) -> pd.Series:
@@ -77,8 +82,11 @@ def disable_copy_warning():
 
 
 def fillna_series(series: pd.Series):
-    if pd.api.types.is_number(series):
+    if pd.api.types.is_numeric_dtype(series):
         return series.fillna(value=_NA_FILL_INTEGER)
+    series, geo = fill_geoseries(series)
+    if geo:
+        return series
     return series.fillna(value=_NA_FILL_OBJECT)
 
 
@@ -128,3 +136,62 @@ def interval_to_range(interval: pd.Interval):
         return adapt(NumericRange(interval.left, interval.right))
     else:
         raise TypeError("this adapter only works for dates/datetimes and numeric")
+
+
+def period_to_interval(period: pd.Period) -> pd.Interval:
+    return pd.IntervalIndex.from_arrays(
+        period.start_time,
+        period.end_time.values + 1,
+        closed="left",
+    )
+
+
+def df_chunker(frame: Union[pd.DataFrame, pd.Series], size):
+    for pos in range(0, len(frame), size):
+        yield frame.iloc[pos : pos + size]
+
+
+def to_date(ser: pd.Series):
+    ser = ser.astype("Int64", copy=False).astype("str", copy=False)
+    for n, fmt in DATE_FORMATS.items():
+        msk = ser.str.len() == n
+        ser[msk] = pd.to_datetime(ser[msk], format=fmt, cache=True)
+    return ser.convert_dtypes()
+
+
+def get_max_chars_in_common(s):
+    lower_end = 1
+    upper_end = s.str.len().min()
+
+    def good(l):
+        return len(s.str.slice(0, l).unique()) == 1
+
+    if not good(lower_end):
+        return None
+    while True:
+        midpoint = int(np.ceil((lower_end + upper_end) / 2))
+        g = good(midpoint)
+        if g:
+            lower_end = midpoint
+        else:
+            upper_end = midpoint - 1
+        if lower_end == upper_end:
+            break
+    return upper_end
+
+
+def get_common_initial_str(s: pd.Series):
+    if n := get_max_chars_in_common(s):
+        return s.head(1).str.slice(0, n)[0]
+    return None
+
+
+def _infer_date(s: pd.Series):
+    return all(isinstance(a, date) for a in s.dropna())
+
+
+def convert_df_dates_to_timestamps(frame: pd.DataFrame):
+    obj_cols = frame.select_dtypes("object").columns
+    date_test = frame[obj_cols].apply(_infer_date)
+    date_cols = date_test[date_test].index
+    frame[date_cols] = frame[date_cols].apply(pd.to_datetime)
