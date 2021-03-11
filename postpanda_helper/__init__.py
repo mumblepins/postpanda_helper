73a5063b6a4ecdab32b550c96fe4a5127d434697
Sullivan <57496265+ds-mn@users.noreply.github.com>
2021-03-11 11:14:21 -0600

    various pandas -> sql improvements, refactoring, added normalizer

--- postpanda_helper/__init__.py
+++ postpanda_helper/__init__.py
@@ -1,92 +1,21 @@
 # coding=utf-8
-from typing import Any, Optional, MutableMapping
 
-import numpy as np
-import pandas as pd
-from psycopg2.extensions import AsIs, adapt, register_adapter
-from sqlalchemy.dialects.postgresql import DATERANGE, NUMRANGE, TSRANGE, TSTZRANGE
+from numpy import int64 as _int64
+from pandas import NA as _NA, Interval as _Interval
+from psycopg2.extensions import (
+    AsIs as _AsIs,
+    adapt as _adapt,
+    register_adapter as _register_adapter,
+)
 
-from .geo_helpers import HAS_GEO_EXTENSIONS
-from .pd_helpers import is_date_interval, interval_to_range
+from .pandas_reader import PandasReader
+from .pandas_writer import PandasWriter
+from .pd_helpers import interval_to_range as _interval_to_range
 from .psql_helpers import PandaCSVtoSQL
 from .select_sert import SelectSert
 
-if HAS_GEO_EXTENSIONS:
-    from .geo_helpers import get_geometry_type, geometry_to_ewkb
+_register_adapter(type(_NA), lambda x: _adapt(None))
+_register_adapter(_int64, lambda x: _AsIs(x))
+_register_adapter(_Interval, _interval_to_range)
 
-    # noinspection PyPackageRequirements
-    from geopandas.array import GeometryDtype
-
-    # noinspection PyPackageRequirements
-    from geopandas import GeoSeries
-
-    # noinspection PyPackageRequirements
-    from geoalchemy2.types import Geometry
-
-register_adapter(type(pd.NA), lambda x: adapt(None))
-register_adapter(np.int64, lambda x: AsIs(x))
-register_adapter(pd.Interval, interval_to_range)
-
-
-def pd_to_sql(
-    frame: pd.DataFrame,
-    name: str,
-    con,
-    schema: Optional[str] = None,
-    if_exists="fail",
-    index=True,
-    index_label=None,
-    chunksize=None,
-    dtype: Optional[MutableMapping[str, Any]] = None,
-    method=None,
-):
-    """
-    With support for daterange and tsrange
-
-    """
-    dtm = dtype or {}
-    copied = False
-    columns = frame.columns
-    for c in columns:
-        if c in dtype:
-            continue
-        if pd.api.types.is_interval_dtype(frame[c]):
-            ex = frame[c][frame[c].first_valid_index()]
-            if isinstance(ex.left, pd.Timestamp):
-                if is_date_interval(ex):
-                    dtm[c] = DATERANGE
-                elif hasattr(ex, "tz") and ex.tz is not None:
-                    dtm[c] = TSTZRANGE
-                else:
-                    dtm[c] = TSRANGE
-            elif pd.api.types.is_number(ex.left):
-                dtm[c] = NUMRANGE
-        elif HAS_GEO_EXTENSIONS and isinstance(frame[c].dtype, GeometryDtype):
-            # copy because we're going to mess with the column
-            if not copied:
-                frame = frame.copy()
-            s = GeoSeries(frame[c])
-            try:
-                geometry_type, has_curve = get_geometry_type(s)
-            except ValueError:
-                frame[c] = frame[c].astype("object")
-                continue
-            srid = s.crs.to_epsg(min_confidence=25) or -1
-            dtm[c] = Geometry(geometry_type=geometry_type, srid=srid)
-            frame[c] = geometry_to_ewkb(frame[c], srid)
-
-    dtm = dtm or None
-    return frame.to_sql(
-        name=name,
-        con=con,
-        schema=schema,
-        if_exists=if_exists,
-        index=index,
-        index_label=index_label,
-        chunksize=chunksize,
-        dtype=dtm,
-        method=method,
-    )
-
-
-__all__ = ["PandaCSVtoSQL", "SelectSert", "pd_to_sql"]
+__all__ = ["PandaCSVtoSQL", "SelectSert", "PandasReader", "PandasWriter"]
--- postpanda_helper/geo_helpers.py
+++ postpanda_helper/geo_helpers.py
@@ -1,14 +1,25 @@
 # coding=utf-8
+from collections import MutableMapping
+from typing import Optional
+
 from pandas import DataFrame, Series
 from sqlalchemy import Table
+from sqlalchemy.sql.type_api import UserDefinedType
 
 HAS_GEO_EXTENSIONS = False
 
 try:
-    from ._geo_helpers import _df_to_shape, _fill_geoseries, get_geometry_type, geometry_to_ewkb
+    from ._geo_helpers import (
+        _df_to_shape,
+        _fill_geoseries,
+        get_geometry_type,
+        geometry_to_ewkb,
+        _convert_geometry_for_postgis,
+    )
 
     df_to_shape = _df_to_shape
     fill_geoseries = _fill_geoseries
+    convert_geometry_for_postgis = _convert_geometry_for_postgis
     HAS_GEO_EXTENSIONS = True
 except ImportError:
 
@@ -17,3 +28,8 @@ except ImportError:
 
     def fill_geoseries(s: Series) -> (Series, bool):
         return s, False
+
+    def convert_geometry_for_postgis(
+        frame: DataFrame, column: str, in_place: bool = False
+    ) -> (Optional[DataFrame], MutableMapping[str, UserDefinedType]):
+        return None, {}
